a8e72247234bdac7fd655771a8ac0cdf8650960c
ximoThorn <32925631+ximoThorn@users.noreply.github.com>
2020-12-04 14:46:34 +0800

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,3 +16,16 @@ export function patchChildren (ctx, componentName, ignoreComponentNames = []) {
     }
   }, []);
 }
+
+export function accMul(arg1, arg2) {
+  let m = 0
+  let s1 = arg1.toString()
+  let s2 = arg2.toString()
+  try {
+    m += s1.split('.')[1].length
+  } catch (e) {}
+  try {
+    m += s2.split('.')[1].length
+  } catch (e) {}
+  return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m)
+}
